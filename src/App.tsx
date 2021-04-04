ae45af68a450b896c26a00e2b2e42cb3d98372d5
Eugene Burtsev <eugene@burtsev.net>
2021-04-04 19:22:01 +0300

    lesson-8

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,46 @@
-import React, { FC } from "react";
+import React, { Component, ReactNode } from "react";
 
 import { Field } from "./Field";
 
-export const App: FC<unknown> = () => {
-    return <Field height={10} width={30} />;
-};
+export interface GameConfig {
+    width: number;
+    height: number;
+}
+
+export interface GameState {
+    config: GameConfig;
+}
+
+async function loadGameConfig(): Promise<GameConfig> {
+    return new Promise<GameConfig>((resolve) => {
+        setTimeout(() => {
+            resolve({ width: 30, height: 10 });
+        }, Math.random() * 5000);
+    });
+}
+
+export class App extends Component<undefined, GameState> {
+    state: Readonly<GameState>;
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            config: {
+                width: 9,
+                height: 3,
+            },
+        };
+    }
+
+    async componentDidMount() {
+        this.setState({ ...this.state, config: await loadGameConfig() });
+    }
+
+    render(): ReactNode {
+        return (
+            <>
+                <Field width={this.state.config.width} height={this.state.config.height} />
+            </>
+        );
+    }
+}
